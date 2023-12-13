ccb95af0bd7468661f25f2fb916454d4413d9c25
mayaassem5 <mayaassem5@gmail.com>
2023-12-13 10:37:49 +0200

    edit prev in pall

--- op_functions.c
+++ op_functions.c
@@ -34,10 +34,10 @@ void pall(stack_t **stack, unsigned int line_num)
 	stack_t *tmp = *stack;
 	(void)line_num;
 
-	while (tmp)
+	while (tmp != NULL)
 	{
-		printf("%d\n", tmp->n);
-		tmp = tmp->next;
+		fprintf(stdout, "%d\n", temp->n);
+		tmp = tmp->prev;
 	}
 }
 
