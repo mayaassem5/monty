9e7d0fca0f051008377f454a5106ec27d8f11be9
mayaassem5 <mayaassem5@gmail.com>
2023-12-13 15:47:01 +0200

    add func

--- op_functions2.c
+++ op_functions2.c
@@ -7,19 +7,19 @@
  **/
 void add(stack_t **stack, unsigned int line_num)
 {
-	stack_t *temp;
+	stack_t *tmp;
 
-	if (!(*stack) || !(*stack)->next)
+	if (!(*stack) || !(*stack)->prev)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 
-	temp = *stack;
-	(*stack) = (*stack)->next;
-	(*stack)->n += temp->n;
-	(*stack)->prev = temp->prev;
-	free(temp);
+	tmp = *stack;
+	(*stack) = (*stack)->prev;
+	(*stack)->n += tmp->n;
+	(*stack)->next = NULL;
+	free(tmp);
 }
 
 /**
